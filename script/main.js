8b1a758d6daddf65c9587f3dd0bf22fe487c398d
zahrarezaie81 <zahrarezaie7674@gmail.com>
2024-05-11 00:36:28 +0330

    some changes in css

--- script/main.js
+++ script/main.js
@@ -1,11 +1,16 @@
 const restart = document.querySelector('.submit');
+const result = document.getElementById('result');
 restart.addEventListener('click',()=>{
 	restart.innerText = 'PLAY AGAIN';
+	result.innerText = 'DRAW';
 	game();
 	restart.addEventListener('click', () => {
 		window.location.reload();
 	})
 })
+
+
+
 const game = () =>{
     let copmuter_score = 0;
     let user_score = 0;
@@ -16,7 +21,7 @@ const game = () =>{
     const computer_action = ['rock', 'paper', 'scissors'];
     const playGame = () =>{
         user_action.forEach(action => {
-			action.addEventListener('click', function () {
+			action.addEventListener('click', () => {
 				const actionList = action.classList;
 				const actionClassName = actionList[0];
 				const num = Math.floor(Math.random()*3);
@@ -27,7 +32,6 @@ const game = () =>{
     }
 
 	const winner = (user, computer) => {
-		const result = document.getElementById('result');
 		const playerScoreBoard = document.querySelector('.user-score');
 		const computerScoreBoard = document.querySelector('.computer-score');
         const report = document.querySelector('.report');
@@ -36,12 +40,14 @@ const game = () =>{
 		if (user == 'rock') {
 			if (computer == 'paper') {
 				result.innerText = 'You Lost!';
+				result.style.color = "red";
                 report.textContent = 'computer choosed paper'
 				copmuter_score++;
 				computerScoreBoard.textContent = copmuter_score.toString();
 
 			} else {
 				result.innerText = 'You Won!'
+				result.style.color = "green";
                 report.textContent = 'computer choosed scissor'
 				user_score++;
 				playerScoreBoard.textContent = user_score.toString();
@@ -50,11 +56,13 @@ const game = () =>{
 		else if (user == 'scissor') {
 			if (computer == 'rock') {
 				result.innerText = 'You Lost!';
+				result.style.color = "red";
                 report.textContent = 'computer choosed rock'
 				copmuter_score++;
 				computerScoreBoard.textContent = copmuter_score.toString();
 			} else {
 				result.innerText = 'You Won!';
+				result.style.color = "green";
                 report.textContent = 'computer choosed paper'
 				user_score++;
 				playerScoreBoard.textContent = user_score.toString();
@@ -64,12 +72,12 @@ const game = () =>{
 			if (computer == 'scissors') {
 				result.innerText = 'You Lost!';
 				result.style.color = "red";
-
                 report.textContent = 'computer choosed scissor'
 				copmuter_score++;
 				computerScoreBoard.textContent = copmuter_score.toString();
 			} else {
 				result.innerText = 'You Won!';
+				result.style.color = "green";
                 report.textContent = 'computer choosed rock'
 				user_score++;
 				playerScoreBoard.textContent = user_score.toString();
